f9e23814a3fae82aec0c2e16daf65deeac36da7c
Volodymyr Boichuk <bodanusya@gmail.com>
2023-04-02 14:04:52 +0300

    Initial commit

---
+++ src/main/java/com/zlagod/ZlagodaApplication.java
@@ -0,0 +1,13 @@
+package com.zlagod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZlagodaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZlagodaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zlagod/ZlagodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlagod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZlagodaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
