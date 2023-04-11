9d61a466c12d4c02b49957edd871766398ae2b7a
Stas <stbukovskiy12@gmail.com>
2023-04-11 21:35:45 +0300

    Refactor BirthDateValidator

--- src/main/java/com/zlagoda/constraints/BirthDateValidator.java
+++ src/main/java/com/zlagoda/constraints/BirthDateValidator.java
@@ -6,11 +6,14 @@ import jakarta.validation.ConstraintValidatorContext;
 import java.util.Calendar;
 import java.util.Date;
 
-public class BirthDateValidator implements ConstraintValidator<BirthDate, Date> {
+import static com.zlagoda.utils.DateConverter.convertToDate;
+
+public class BirthDateValidator implements ConstraintValidator<BirthDate, String> {
     @Override
-    public boolean isValid(final Date valueToValidate, final ConstraintValidatorContext context) {
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        final Date dateToValidate = convertToDate(value);
         Calendar dateInCalendar = Calendar.getInstance();
-        dateInCalendar.setTime(valueToValidate);
+        dateInCalendar.setTime(dateToValidate);
 
         return Calendar.getInstance().get(Calendar.YEAR) - dateInCalendar.get(Calendar.YEAR) >= 18;
     }
