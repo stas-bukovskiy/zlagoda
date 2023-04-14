058ef514ca18c83b8fc019ecb81208e3112cfe42
Stas <stbukovskiy12@gmail.com>
2023-04-14 13:32:29 +0300

    Add 404 and 403 error handling

---
+++ src/main/java/com/zlagoda/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.zlagoda.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = NotFoundException.class)
+    public ModelAndView handleNotFoundException(NotFoundException e) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", e);
+        modelAndView.setViewName("error/404");
+
+        log.debug("[EXCEPTION_HANDLER] handled {}", e.getClass().getName());
+        return modelAndView;
+    }
+
+}
